972bb7197f96cd656e63d7cd8da9f0f185a0a2fb
Vins <vinskaspian@gmail.com>
2025-06-21 21:07:58 +0200

    write-code

---
+++ task1/task-1-theme-6.py
@@ -0,0 +1,45 @@
+def total_salary(path):
+    """
+    Parses a text file with developer salaries and calculates
+    the total and average salary.
+    
+    Args:
+    path (str): Path to the txt file containing salary data.
+    Each line has the format "LastNameFirstName,salary".
+    
+    Returns:
+    tuple: A tuple of two numbers:
+    (total_salary, average_salary).
+    Returns (0, 0) in case of file error or if the file is empty/does not contain valid data.
+    """
+    total_sum = 0
+    development_count =0
+    try:
+        with open(path,'r',encoding='utf-8') as file:
+            for line in file:
+                stripped_line = line.strip()
+                if not stripped_line:
+                    continue
+                parts = stripped_line.split(',')
+                if len (parts) == 2:
+                    try:
+                        salary_str = parts[1]
+                        salary = int(salary_str)
+
+                        total_sum += salary
+                        developer_count += 1
+                    except ValueError : 
+                        print(f"Incorrect salary format in line: '{stripped_line}'. Skipping.")
+                    else:
+                        print(f"Invalid line format: '{stripped_line}'. Skipping.")
+    except FileNotFoundError :
+            print (f"File not found at path '{path}'.")
+            return 0,0
+    except Exception as e :
+            print(f"An unknown error occurred while reading file '{path}': {e}") 
+            return 0,0 
+    
+    average_salary = total_sum / developer_count  if  developer_count > 0 else 0
+    
+    return (total_sum / average_salary)
+
